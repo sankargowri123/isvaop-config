048eb9a742ab5c12e4ec576168af22beaf567342
sankargowri123 <75351824+sankargowri123@users.noreply.github.com>
2025-12-23 12:11:43 +0700

    Add files via upload

--- accesspolicies/default_policy.js
+++ accesspolicies/default_policy.js
@@ -1,28 +1,72 @@
-importClass(Packages.com.tivoli.am.fim.trustserver.sts.utilities.IDMappingExtUtils);
-IDMappingExtUtils.traceString("Starting Access Policy JS");
+importClass(Packages.com.ibm.security.access.policy.decision.Decision);
+importClass(Packages.com.ibm.security.access.policy.decision.RedirectChallengeDecisionHandler);
+importPackage(Packages.com.tivoli.am.fim.trustserver.sts.utilities);
+/*Pre-requisite steps
+1) We are using the username password authentication mechanism. Hence Advanced Access Control has to be activated and run isamcfg or isam aac config. The junction used for isamcfg is /mga.
+2) We modify the AuthSvcCredential mapping rule to add an attribute called authenticationTime.
+Example
+stsuuAttrs.setAttribute(new Attribute("authenticationTime", null, (new Date()).getTime()));
+3) Since we are using the username password authentication mechanism, remove the isam_oauth_anyauth acl from the /mga/sps/auth endpoint and attach an isam_oauth_unauth acl.(Assuming /mga is the junction created using the OAuth OIDC configuration)
+4) Change the <ServerIP> to the IP address of the reverse proxy that will perform the username password authentication.
 
-var use_external_consent = false;
+*/
+var prompt_login = false;
+var protocolContext = context.getProtocolContext();
+//Checking if prompt is requested by retrieveing the authentication context.
+var promptVal = protocolContext.getAuthenticationRequest().getAuthenticationContext().getPrompt();
+if (promptVal != null) {
+	prompt_login = true;
+}
+//Function used to call username password authentication mechanism
+function getRedirectToAuthSvc() {
+	var handler = new RedirectChallengeDecisionHandler();
+	IDMappingExtUtils.traceString("rediect to authSvc");
+	handler.setRedirectUri("/eai/web/v1/auth/login");
+	IDMappingExtUtils.traceString("returning challenge");
+	return handler;
+}
 
-if (context.getRequest().getHeader("x-origin-ip") == "1.2.3.4") {
-    /**
-     * Here is an example of doing deny page response
-     */
-    var handler = new HtmlPageDenyDecisionHandler();
-    handler.setMacro("@ERROR_CODE@", "access_denied");
-    handler.setMacro("@ERROR_DESCRIPTION@", "Request coming from forbidden IP address.");
-    handler.setPageId("user_error.html");
-    context.setDecision(Decision.deny(handler));
-} else {
-    /**
-     * Using external consent:
-     * In the access policy javascript, you can response with a page challenge which
-     * show a consent page; or it is possible also to redirect to external consent system.
-     * After the consent is collected, the information of consented scope can be passed on
-     * by calling 'setConsentDecision()'
-     */
-    if (use_external_consent) {
-        var extScopes = ["openid", "email"];
-        context.getProtocolContext().setConsentDecision(extScopes);
-    }
-    context.setDecision(Decision.allow());
+if (prompt_login) {
+	context.setDecision((function () {
+		var request = context.getRequest();
+
+		var user = context.getUser();
+		if (user == null) {
+			IDMappingExtUtils.traceString("User isnt authenticated");
+			return Decision.challenge(getRedirectToAuthSvc());
+		} else {
+			// The current HTTP request contains authentication request.
+			IDMappingExtUtils.traceString("Currently authenticated user: " + user);
+			var responseType = request.getParameter("response_type");
+			if (responseType == null) {
+			} else {
+				// User is authenticated.
+				IDMappingExtUtils.traceString("Prompt is: " + promptVal);
+				if (promptVal != null && promptVal.contains("login")) {
+					return Decision.challenge(getRedirectToAuthSvc());
+				} else {
+					IDMappingExtUtils.traceString("Allowed");
+					return Decision.allow();
+				}
+			}
+		}
+
+		IDMappingExtUtils.traceString("Allowed");
+		return Decision.allow();
+	})());
+}
+else {
+	//If neither promot or max_age is requested, we retrieve user context
+	context.setDecision((function () {
+		var user = context.getUser();
+		var auth_time;
+		IDMappingExtUtils.traceString("user  :" + user);//Authenticate using username password mechanism if there is no user session
+		if (user == null) {
+			IDMappingExtUtils.traceString("User Session is unavailable");
+			return Decision.challenge(getRedirectToAuthSvc());
+		}
+		else {
+			return Decision.allow();
+		}
+	})());
 }
